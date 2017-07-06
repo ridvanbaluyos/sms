e14f474035a29d4c87a1920773f74aa4fc6765aa
Ridvan Baluyos <ridvan@baluyos.net>
2017-07-06 16:17:52 +0800

    Upgrade to Semaphore API Version 4.

--- src/ridvanbaluyos/sms/providers/Semaphore.php
+++ src/ridvanbaluyos/sms/providers/Semaphore.php
@@ -33,34 +33,37 @@ class Semaphore extends Sms implements SmsProviderServicesInterface
      * @return string
      */
     public function send($phoneNumber, $message)
-    {
+    {      
         try {
             $conf = Config::load(__DIR__ . '/../config/providers.json')[$this->className];
             $query = [
-                'from' => $conf['from'],
-                'api' => $conf['api'],
+                'sendername' => $conf['sendername'],
+                'apikey' => $conf['apikey'],
                 'number' => $phoneNumber,
                 'message' => $message,
             ];
 
-            $url = $conf['url'] . '?' . http_build_query($query);
-
             $ch = curl_init();
-            curl_setopt($ch,CURLOPT_URL,$url);
+            curl_setopt($ch, CURLOPT_URL, $conf['url']);
+            curl_setopt($ch, CURLOPT_POST, 1 );
+            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($query));
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_TIMEOUT, 240);
+            
             $result = curl_exec($ch);
-            curl_close($ch);
+            curl_close ($ch);
 
             $result = json_decode($result);
 
-            if ($result->status === 'success') {
-                return $this->response(202, $result, null, $this->className);
-            } else {
-                return $this->response(500, $result->message, null, $this->className);
+            if (isset($result)) {
+                if (is_object($result)) {
+                    return $this->response(400, $result, null, $this->className);
+                } else {
+                    return $this->response(200, $result, null, $this->className);
+                }
             }
         } catch (Exception $e) {
-
+            var_dump($e);
+            exit;
         }
     }
 
